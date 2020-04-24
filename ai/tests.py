03b14f0fec776758d7190187fd1e318e4a025fbe
Sanico <salocin.sam@gmail.com>
2020-04-24 17:06:04 +0200

    first commit

---
+++ ai/tests.py
@@ -0,0 +1,23 @@
+import socket
+
+HEADER = 4096
+PORT = 3001
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "localhost"
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(4096).decode(FORMAT))
+
+
+send(DISCONNECT_MESSAGE)
